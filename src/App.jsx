c92decb189c45065d5f680eaeb9b7e841fa645cc
tamazm <mdzhavanadzetoma@gmail.com>
2024-07-20 22:50:22 +0400

    added quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import SlotsMachine4 from './components/SlotsMachine4'
+import SlotsMachine4 from './components/SlotsMachine4';
+import QuizzGame3 from './components/QuizGame3';
 
 function App() {
 
   return (
     <>
-      <SlotsMachine4 />
+    <QuizzGame3/>
     </>
   )
 }
